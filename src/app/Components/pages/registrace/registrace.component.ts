8d7c92bef5d6e4d0416fc0a4c7d4bcab7988ddab
Honzam5 <marcinjan@sssvt.cz>
2025-04-14 18:31:55 +0200

    registrace(zatim nefunguje) a login(funguje)

--- src/app/Components/pages/registrace/registrace.component.ts
+++ src/app/Components/pages/registrace/registrace.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrace',
-  imports: [],
   templateUrl: './registrace.component.html',
-  styleUrl: './registrace.component.scss'
+  styleUrls: ['./registrace.component.scss']
 })
 export class RegistraceComponent {
+  constructor(private router: Router) {}
 
+  // Můžete přidat metodu pro navigaci zpět na přihlášení
+  goToLogin() {
+    this.router.navigate(['/prihlaseni']);
+  }
 }

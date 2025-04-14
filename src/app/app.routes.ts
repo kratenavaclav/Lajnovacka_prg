ca48c79a2bbcd2d27450ad01602568ab7cc5b6d7
kratenavaclav <kratenavaclav@sssvt.cz>
2025-04-14 17:34:56 +0200

    novy team list more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { KontaktComponent } from './Components/pages/kontakt/kontakt.component';
 import { TabulkaLigyComponent } from './Components/pages/tabulka-ligy/tabulka-ligy.component';
 import { TabulkaStrelcuComponent } from './Components/pages/tabulka-strelcu/tabulka-strelcu.component';
 import { TipovaciSoutezComponent } from './Components/pages/tipovaci-soutez/tipovaci-soutez.component';
+import { PrihlaseniComponent } from './Components/pages/prihlaseni/prihlaseni.component';
+import { RegistraceComponent } from './Components/pages/registrace/registrace.component';
 
-import { PrihlaseniComponent } from './Components/pages/prihlaseni/prihlaseni.component'; // Import přihlašovací komponenty
+
 
 export const routes: Routes = [
   { path: '', component: HlavniStrankaComponent }, // Výchozí stránka
@@ -15,7 +17,8 @@ export const routes: Routes = [
   { path: 'tabulka-ligy', component: TabulkaLigyComponent },
   { path: 'tabulka-strelcu', component: TabulkaStrelcuComponent },
   { path: 'tipovaci-soutez', component: TipovaciSoutezComponent },
-
-  { path: 'prihlaseni', component: PrihlaseniComponent }, // Trasa pro přihlášení
+  { path: 'prihlaseni', component: PrihlaseniComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }, // Přesměrování na hlavní stránku při neplatné URL
+  { path: 'registrace', component: RegistraceComponent }, // New route for registration
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for invalid URLs
 ];

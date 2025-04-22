73a2d78dc5e9ecbebae2672e2fc69cbbc3d362cf
Honzam5 <marcinjan@sssvt.cz>
2025-04-22 22:16:07 +0200

    nefunguje propojeni porad

--- src/app/Components/pages/hlavni-stranka/hlavni-stranka.component.ts
+++ src/app/Components/pages/hlavni-stranka/hlavni-stranka.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-hlavni-stranka',
   standalone: true,
-  imports: [NavbarComponent], // Importuje navigační lištu
+  imports: [NavbarComponent, RouterLink], // Importuje navigační lištu
   templateUrl: './hlavni-stranka.component.html',
   styleUrls: ['./hlavni-stranka.component.scss']
 })

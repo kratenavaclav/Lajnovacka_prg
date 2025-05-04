e994503531b1a2cc008d64de010a23eecb251959
Honzam5 <marcinjan@sssvt.cz>
2025-05-04 20:41:03 +0200

    liberec sparta slavie hotovo

--- src/app/Components/team-details/liberec/liberec/liberec.component.ts
+++ src/app/Components/team-details/liberec/liberec/liberec.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from '../../../navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../../navbar/navbar.component';
+import { TeamService } from 'app/services/teams/team-service';
+import { Player } from 'app/models/player/model';
+import { Coach } from 'app/models/coach/model';
+import { Team } from 'app/models/teams/models';
 
 @Component({
   selector: 'app-liberec',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './liberec.component.html',
-  styleUrl: './liberec.component.scss'
+  styleUrls: ['./liberec.component.scss']
 })
-export class LiberecComponent {
+export class LiberecComponent implements OnInit {
+  goalkeepers: Player[] = [];
+  defenders: Player[] = [];
+  midfielders: Player[] = [];
+  forwards: Player[] = [];
 
+  coach: Coach = {
+    id: 0,
+    name: '',
+    surname: '',
+    teamId: 0,
+    nationality: ''
+  };
+
+  team: Team = {
+    id: 0,
+    name: '',
+    logo: null,
+    stadium: '',
+    stadiumCapacity: 0,
+    foundationYear: 0,
+    leagueTitles: 0,
+    cupTitles: 0
+  };
+
+  constructor(private teamService: TeamService) {}
+
+  ngOnInit(): void {
+    const teamId = 1;
+
+    this.teamService.getPlayersByTeamId(teamId).subscribe(players => {
+      this.goalkeepers = players.filter(p => p.position?.toLowerCase() === 'goalkeeper');
+      this.defenders = players.filter(p => p.position?.toLowerCase() === 'defender');
+      this.midfielders = players.filter(p => p.position?.toLowerCase() === 'midfielder');
+      this.forwards = players.filter(p => p.position?.toLowerCase() === 'forward');
+    });
+
+    this.teamService.getCoachByTeamId(teamId).subscribe(c => {
+      if (c) this.coach = c;
+    });
+
+    this.teamService.getTeamById(teamId).subscribe(t => {
+      if (t) this.team = t;
+    });
+  }
 }

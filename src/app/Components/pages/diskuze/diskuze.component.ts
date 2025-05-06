acf894edb1792924d3fabb88ab0c6f54f481f73f
kratenavaclav <kratenavaclav@sssvt.cz>
2025-05-06 09:32:46 +0200

    dokonceni checkpointu(diskuze)

--- src/app/Components/pages/diskuze/diskuze.component.ts
+++ src/app/Components/pages/diskuze/diskuze.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from "../../navbar/navbar.component";
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../../navbar/navbar.component';
 
 @Component({
   selector: 'app-diskuze',
+  standalone: true,
   templateUrl: './diskuze.component.html',
-  imports: [NavbarComponent], // Importuje navigační lištu
-  styleUrls: ['./diskuze.component.scss']
+  styleUrls: ['./diskuze.component.scss'],
+  imports: [CommonModule, FormsModule, NavbarComponent]
 })
-export class DiskuzeComponent {
+export class DiskuzeComponent  implements OnInit {
+  teams = [
+    'SK Slavia Praha',
+    'AC Sparta Praha',
+    'FC Viktoria Plzeň',
+    'FC Baník Ostrava',
+    'SK Sigma Olomouc',
+    'FC Slovácko',
+    'Bohemians Praha 1905',
+    'FK Jablonec',
+    'FK Mladá Boleslav',
+    'FC Hradec Králové',
+    'FC Zlín',
+    'FK Teplice',
+    'Dynamo České Budějovice',
+    'Karviná',
+    'Pardubice',
+    'FK Jihlava'
+  ];
+  selectedTeam: string | null = null;
+  teamMessages: Record<string, string[]> = {};
+  newMessage = '';
 
+  ngOnInit(): void {
+    const savedMessages = localStorage.getItem('teamMessages');
+    if (savedMessages) {
+      this.teamMessages = JSON.parse(savedMessages);
+    }
+  }
+
+  get messages(): string[] {
+    if (this.selectedTeam) {
+      return this.teamMessages[this.selectedTeam] || [];
+    }
+    return [];
+  }
+
+  selectTeam(team: string) {
+    this.selectedTeam = team;
+    if (!this.teamMessages[team]) {
+      this.teamMessages[team] = [];
+    }
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim() && this.selectedTeam) {
+      this.teamMessages[this.selectedTeam].push(this.newMessage.trim());
+      this.newMessage = '';
+      localStorage.setItem('teamMessages', JSON.stringify(this.teamMessages));
+    }
+  }
 }

7f05c9d09dff20c98be9f6fc30c84c12abe63f48
kratenavaclav <kratenavaclav@sssvt.cz>
2025-05-23 09:45:16 +0200

    doplneni moje sazecky

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,8 @@ import { TipovaciSoutezComponent } from './Components/pages/tipovaci-soutez/tipo
 import { PrihlaseniComponent } from './Components/pages/prihlaseni/prihlaseni.component';
 import { RegistraceComponent } from './Components/pages/registrace/registrace.component';
 import { TeamDetailsComponent } from './Components/team-details/team-details.component';
-import { PlayerComponent } from './Components/pages/player/player.component'; // <-- nový import
+import { PlayerComponent } from './Components/pages/player/player.component';
+import { MojeSazeckyPageComponent } from './Components/pages/Moje-sazecky-page/Moje-sazecky-page.component';
 
 export const routes: Routes = [
   { path: '', component: HlavniStrankaComponent },
@@ -18,6 +19,7 @@ export const routes: Routes = [
   { path: 'tabulka-strelcu', component: TabulkaStrelcuPageComponent },
   { path: 'tipovaci-soutez', component: TipovaciSoutezComponent },
   { path: 'teams/:teamId', component: TeamDetailsComponent },
+  {path: 'moje-sazecky-page', component: MojeSazeckyPageComponent},
   { path: 'hraci/:id', component: PlayerComponent },
   { path: 'prihlaseni', component: PrihlaseniComponent },
   { path: 'registrace', component: RegistraceComponent },
